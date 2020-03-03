f568da8a9fedc756713974b9ac29f2185cdb1eb4
Martin <cph-ms782@cphbusiness.dk>
2020-03-03 12:10:37 +0100

    exercise 6

--- 06-Exercise/NotFoundException.py
+++
@@ -1,3 +0,0 @@
-class NotFoundException(Exception):
-    """Raised when the url returns value 404"""
-    print("url not found"
--- 06-Exercise/exercise6_1.py
+++ 06-Exercise/exercise6_1.py
@@ -2,21 +2,31 @@
 
 # Create a module containing a class with the following methods:
 # import webget
-# import NotFoundException
 import pandas as pd
 from requests import get  # to make GET request
+from concurrent.futures import ThreadPoolExecutor
+import requests
+
+
+class NotFoundException(ValueError):
+    """Raised when the url returns value 404"""
+
+    def __init__(self, *args, **kwargs):
+        ValueError.__init__(self, *args, **kwargs)
 
 
 class Exercise6_1:
     # 1. init(self, url_list)
     def __init__(self, url_list):
         self.url_list = url_list
+        self.filenames = []
 
-    # 5. urlist_iterator(url_list) returns an iterator to loop through the urls in the list
+    # 4. iter() returns an iterator
     def __iter__(self):
         self.index = 0
         return self
 
+    # 5. next() returns each of the downloaded files
     def __next__(self):
         index = self.index
         if index >= len(self.url_list):
@@ -26,6 +36,7 @@ class Exercise6_1:
         return self.url_list[index]
 
     # 2. download(url, filename) raises NotFoundException when url returns 404
+    # What is NotFoundException ???
     def download(self, url, filename):
         print("url", url)
         print("filename", filename)
@@ -33,25 +44,30 @@ class Exercise6_1:
         with open(filename, "wb") as file:
             # get request
             try:
-                response = get(url)
+                r = requests.get(url)
+                if r.status_code == 404:
+                    raise NotFoundException("URL: ", url, " is not working. Status code 404")
                 # write to file
-                file.write(response.content)
+                file.write(r.content)
                 print("file downloaded")
-            except:
-                print("NotFoundException")
+            except ConnectionError as ex:
+                print(ex)
+            except NotFoundException as ex:
+                print(ex)
+            except Exception as ex:
+                print(ex)
 
     # 3. multi_download(url_list) uses threads to download multiple urls as text and stores filenames as a property
     def multi_download(self, url_list):
+        workers = 4
         with ThreadPoolExecutor(workers) as ex:
-            res = ex.map(self.download(url, index), range(4), 4)
-        return list(res)
+            urls = [url_list[x] for x in range(len(url_list))]
+            filenames = [str(y)+".txt" for y in range(len(url_list))]
+            ex.map(self.download, urls, filenames)
+        return self.filenames
 
-    # # 4. urlist_generator(url_list) returns a generator to loop through the filenames
-    # def urlist_generator(url_list):
-    #     pass
-
-    # 6. load_files() returns generator to yield each of the downloaded files
-    def load_files(self):
+    # 6. filelist_generator(url_list) returns a generator to loop through the filenames
+    def filelist_generator(self, ):
         pass
 
     # 7. avg_vowels(text) - a rough estimate on readability returns average number of vowels in the words of the text
@@ -77,7 +93,14 @@ if __name__ == '__main__':
         except StopIteration:
             # if StopIteration is raised, break from loop
             break
-    url = "https://www.gutenberg.org/files/1342/1342-0.txt"
 
     filing = Exercise6_1([])
-    filing.download(url, "1342-0.txt")
+
+    url = "https://www.gutenberg.org/files/1342/1342-0.txt"
+    url2 = "https://www.gutenberg.org/files/1343/1343-0.txt"
+    url3 = "https://www.gutenberg.org/files/1344/1344-0.txt"
+
+    url_list = [url, url2, url3]
+    reslist = filing.multi_download(url_list)
+    print("list of filenames: ", reslist)
+    # filing.download(url, "1342-0.txt")
